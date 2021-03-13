b802c07c1b5161b5f99be869a0897a50ad4f562f
张小龙 <zhangxiaolong@tuhu.cn>
2021-03-13 18:24:14 +0800

    13

--- src/views/Patient.vue
+++ src/views/Patient.vue
@@ -1,9 +1,8 @@
 <template>
     <div>
       <Header></Header>
-
-      <el-tabs v-model="activeName" @tab-click="handleClick" style="width: 50%;margin: 0 auto;" type="card" >
-        <el-tab-pane label="立即挂号" name="first" width="30%">
+      <el-tabs v-model="activeName" @tab-click="handleClick" style="width: 57%;margin: 0 auto;" type="card" >
+        <el-tab-pane label="立即挂号" name="nowAppoint" width="30%">
           <el-form ref="form" :model="patientAppointment" label-width="80px">
             <el-form-item label="预约挂号" prop="1">
               <el-switch v-model="appointmentSwitch"></el-switch>
@@ -18,7 +17,9 @@
                 type="date"
                 placeholder="选择日期"
                 value-format="yyyy-MM-dd hh:mm:ss"
-                :picker-options="pickerOptions">
+                :picker-options="pickerOptions"
+                :default-value="mDate"
+              >
               </el-date-picker>
             </el-form-item>
             <el-form-item label="医生" prop="3">
@@ -43,17 +44,68 @@
                   v-for="doctor in doctors"
                   :key="doctor.id"
                   :label="doctor.doctorName"
-                  :value="doctor.id">
+                  :value="doctor.id"
+                  :disabled="doctor.isFull">
+                  {{doctor.doctorName}} {{doctor.nowNum}}/{{doctor.resources}}
                 </el-option>
               </el-select>
             </el-form-item>
             <el-form-item label="病情" prop="4">
-              <el-input type="textarea" v-model="patientAppointment.illness" style="width: 660px;height: 300px;"></el-input>
+              <el-input type="textarea" v-model="patientAppointment.illness" ></el-input>
+            </el-form-item>
+            <el-form-item>
+              <el-button type="primary" @click="patientAppointmentClick">立即预约</el-button>
             </el-form-item>
           </el-form>
         </el-tab-pane>
-        <el-tab-pane label="我的挂号" name="second">配置管理</el-tab-pane>
-        <el-tab-pane label="我的医保卡" name="third">角色管理</el-tab-pane>
+        <el-tab-pane label="我的挂号" name="myAppoint">
+          <el-table
+            :data="MedicalHistoryVo.result"
+            stripe
+            style="width: 100%">
+            <el-table-column
+              prop="user"
+              label="部门"
+              width="180">
+              <template slot-scope="departmentScope">
+                <span>{{departmentScope.row.doctor.department}}</span>
+              </template>
+            </el-table-column>
+            <el-table-column
+              prop="user"
+              label="科室"
+              width="180">
+            </el-table-column>
+            <el-table-column
+              prop="user"
+              label="医生">
+            </el-table-column>
+            <el-table-column
+              prop="user"
+              label="病情">
+            </el-table-column>
+            <el-table-column
+              prop="user"
+              label="状态">
+            </el-table-column>
+            <el-table-column
+              prop="user"
+              label="操作">
+              <template slot-scope="doScope">
+                <el-button type="text" size="small"
+                >编辑</el-button
+                >
+                <el-button
+                  type="text"
+                  size="small"
+                >删除</el-button
+                >
+              </template>
+            </el-table-column>
+          </el-table>
+        </el-tab-pane>
+
+        <el-tab-pane label="我的医保卡" name="myCard">角色管理</el-tab-pane>
       </el-tabs>
     </div>
 </template>
@@ -67,23 +119,30 @@
         data() {
             //这里存放数据",
             return {
+              mDate: new Date().setTime(new Date().getTime()+24*60*60*1000),
               pickerOptions: {
                 disabledDate(time) {
                   return time.getTime() < Date.now();
                 },
               },
+              pageVo:{
+                page:1,
+                pageSize:10,
+              },
+              MedicalHistoryVo:{
+                result:[],
+              },
               departments:[],
               sections:[],
               doctors:[],
               appointmentSwitch:false,
-              activeName: 'first',
+              activeName: 'myAppoint',
               patientApp:{
                 departmentId:null,
                 sectionId:null,
                 appointmentTime:null,
               },
               patientAppointment:{
-                  name:null,
                 illness:null,
                 appointmentTime:null,
                 doctorId:null,
@@ -102,6 +161,7 @@
                   appointmentTime:null,
                 }
                 this.patientAppointment.doctorId = null
+                this.patientAppointment.appointmentTime = null
             }
           },
           "patientApp.departmentId": {
@@ -125,9 +185,41 @@
         },
         //方法集合",
         methods: {
-          formatDate(dateTime){
-            console.log(typeof(dateTime))
-            return date.getFullYear()+"-"+date.getMonth()+"-"+date.getDay()+" 00:00:00"
+          patientAppointmentClick(){
+            //病人获取医生
+            var that = this;
+            var data = JSON.stringify(that.patientAppointment)
+            axios({
+              headers: {
+                'Content-Type': 'application/json'
+              },
+              method: 'post',
+              url: "patient/patientAppointment",
+              data:data
+            }).then(response=>{
+              if(response.data.code == 200 ){
+                this.$message({
+                  message: '预约成功',
+                  type: 'success'
+                });
+                that.activeName = "myAppoint";
+                that.patientApp = {
+                  departmentId:null,
+                    sectionId:null,
+                    appointmentTime:null,
+                };
+                that.patientAppointment = {
+                  illness:null,
+                    appointmentTime:null,
+                    doctorId:null,
+                };
+              }else{
+                console.log("失败")
+              }
+
+            }).catch(error=>{
+              alert("失败")
+            })
           },
           getDepartmentAll(){
             //获取所有部门
@@ -184,8 +276,30 @@
             })
           },
           handleClick(tab, event) {
-            console.log(tab, event);
-          }
+            // console.log(tab, event);
+          },
+          getMedicineHistory(){
+            //病人获取医生
+            var that = this;
+            var data = JSON.stringify(that.pageVo)
+            axios({
+              headers: {
+                'Content-Type': 'application/json'
+              },
+              method: 'post',
+              url: "patient/getMedicineHistory",
+              data:data
+            }).then(response=>{
+              console.log(response)
+              if(response.data.code == 200 ){
+                that.MedicalHistoryVo = response.data.data;
+              }else{
+                console.log("失败")
+              }
+            }).catch(error=>{
+              alert("失败")
+            })
+          },
         },
         //生命周期 - 创建之前",数据模型未加载,方法未加载,html模板未加载
         beforeCreate() {
@@ -203,6 +317,7 @@
         //生命周期 - 挂载完成（可以访问DOM元素）",html模板已渲染
         mounted() {
           this.getDepartmentAll();
+          this.getMedicineHistory();
         },
 
         //生命周期 - 更新之前",数据模型已更新,html模板未更新
@@ -257,7 +372,19 @@
 
 <style>
 .el-textarea__inner{
+  width: 620px;
+  min-width: 300px;
   min-height: 200px !important;
 }
+  .myAppointUl{
+    width: 600px;
+    display: flex;
+  }
+  .myAppointUl li{
+    list-style: none;
+    flex: 1;
+    border-right:3px solid #eee;
+    text-align:center;
+  }
 </style>
 
